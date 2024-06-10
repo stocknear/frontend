3abb89eb1cdad974307524f2c27d2096a2b8998d
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-06-10 17:02:50 +0200

    ui fixes

--- src/lib/components/ShareStatistics.svelte
+++ src/lib/components/ShareStatistics.svelte
@@ -205,7 +205,7 @@ $: {
             </div>
 
             {#if rawData?.sharesShort !== 0}
-            <h2 class="mt-10 mr-1 cursor-pointer flex flex-row items-center text-white text-xl sm:text-2xl font-bold mb-3">
+            <h2 class="mt-10 mr-1 flex flex-row items-center text-white text-xl sm:text-2xl font-bold mb-3">
               Short Selling Information
             </h2>
             <span class="text-white">
