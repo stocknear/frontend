f843c037612c6ef5db18cb92f05efc51e37c358b
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-06-17 10:15:43 +0200

    update pricing page

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -306,11 +306,12 @@ async function purchasePlan() {
                         <svg class="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><mask id="ipSSuccess0"><g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="m24 4l5.253 3.832l6.503-.012l1.997 6.188l5.268 3.812L41 24l2.021 6.18l-5.268 3.812l-1.997 6.188l-6.503-.012L24 44l-5.253-3.832l-6.503.012l-1.997-6.188l-5.268-3.812L7 24l-2.021-6.18l5.268-3.812l1.997-6.188l6.503.012L24 4Z"/><path stroke="#0F0F0F" d="m17 24l5 5l10-10"/></g></mask><path fill="#10db06" d="M0 0h48v48H0z" mask="url(#ipSSuccess0)"/></svg>
                         <span class="text-white text-[1rem] font-semibold">Unlimited Options Data</span>
                     </li>
+                    <!--
                     <li class="flex items-center space-x-3 ">
-                        <!-- Icon -->
                         <svg class="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><mask id="ipSSuccess0"><g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="m24 4l5.253 3.832l6.503-.012l1.997 6.188l5.268 3.812L41 24l2.021 6.18l-5.268 3.812l-1.997 6.188l-6.503-.012L24 44l-5.253-3.832l-6.503.012l-1.997-6.188l-5.268-3.812L7 24l-2.021-6.18l5.268-3.812l1.997-6.188l6.503.012L24 4Z"/><path stroke="#0F0F0F" d="m17 24l5 5l10-10"/></g></mask><path fill="#10db06" d="M0 0h48v48H0z" mask="url(#ipSSuccess0)"/></svg>
                         <span class="text-white text-[1rem] font-semibold">{mode ? '14 Days Free Trial' : '7 Days Free Trial'}  </span>
                     </li>
+                    -->
                 </ol>
                 <div class="divider"></div>
                 <div class="m-auto w-full text-white font-medium text-sm mb-6">
@@ -368,6 +369,7 @@ async function purchasePlan() {
                 <div class="p-4" style="margin-bottom: -20px">
                     <!--Row Starts-->
                     
+                    <!--
                     <details class="collapse collapse-arrow bg-[#131313]">
                         <summary class="collapse-title text-lg font-semibold">How does the free trial work?</summary>
                         <div class="collapse-content"> 
@@ -379,6 +381,8 @@ async function purchasePlan() {
                     </details>
 
                     <hr class="border-gray-800"/>
+                    -->
+
                 
                     <!--    
                     <details class="collapse collapse-arrow bg-[#131313]">
@@ -389,7 +393,6 @@ async function purchasePlan() {
                     </details>
                     -->
 
-                    <hr class="border-gray-800"/>
                     <details class="collapse collapse-arrow bg-[#131313]">
                         <summary class="collapse-title text-lg font-semibold">What is your refund policy?</summary>
                         <div class="collapse-content"> 
