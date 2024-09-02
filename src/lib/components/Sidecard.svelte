a504bf3141f6641ae767d2d227bcb5295016c530
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-09-02 14:33:23 +0200

    fix packages and add cards back again

--- src/lib/components/Sidecard.svelte
+++ src/lib/components/Sidecard.svelte
@@ -3,8 +3,8 @@
 import TickerInfoCard from '$lib/components/TickerInfoCard.svelte';
 import AnalystCard from '$lib/components/AnalystCard.svelte';
 //import ESGCard from '$lib/components/ESGCard.svelte';
-//import SimilarTickerCard from '$lib/components/SimilarTickerCard.svelte';
-//import TopETFTickerHolder from '$lib/components/TopETFTickerHolder.svelte';
+import SimilarTickerCard from '$lib/components/SimilarTickerCard.svelte';
+import TopETFTickerHolder from '$lib/components/TopETFTickerHolder.svelte';
 
 export let data;
 export let stockDeck;
@@ -26,6 +26,6 @@ export let analystRating
 
 <!--<ESGCard stockDeck={stockDeck} />-->
 
-<!--<SimilarTickerCard similarstock={similarstock}/>-->
+<SimilarTickerCard similarstock={similarstock}/>
 
-<!--<TopETFTickerHolder topETFHolder={topETFHolder}/>-->
+<TopETFTickerHolder topETFHolder={topETFHolder}/>
