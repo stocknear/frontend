342e1f7346fa21fc8a626e9e23e855dfdd25e4e3
MuslemRahimi <mrahimi@stocknear.com>
2025-10-19 23:20:25 +0200

    ui fix

---
+++ src/routes/portfolio/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  export let data;
+
+  let activeIdx = 0;
+</script>
+
+<section
+  class="w-full max-w-3xl sm:max-w-[1400px] overflow-hidden pb-20 pt-5 px-3 text-muted dark:text-white"
+>
+  <div class="text-sm sm:text-[1rem] breadcrumbs">
+    <ul>
+      <li><a href="/" class="text-muted dark:text-gray-300">Home</a></li>
+      <li class="text-muted dark:text-gray-300">Portfolio</li>
+    </ul>
+  </div>
+
+  <div class="w-full overflow-hidden m-auto mt-5">
+    <div class="sm:p-0 flex justify-center w-full m-auto overflow-hidden">
+      <div
+        class="relative flex justify-center items-start overflow-hidden w-full"
+      >
+        <main class="w-full">
+          <h1 class=" text-2xl sm:text-3xl font-bold">Portfolio</h1>
+
+          <slot />
+        </main>
+      </div>
+    </div>
+  </div>
+</section>
