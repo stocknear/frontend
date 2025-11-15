84d2af5cbe5f68243948f633e375763bf86cd981
MuslemRahimi <mrahimi@stocknear.com>
2025-11-15 20:00:28 +0100

    update security

--- src/routes/api/download-checker/+server.ts
+++ src/routes/api/download-checker/+server.ts
@@ -1,18 +1,27 @@
 import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async ({ locals }) => {
-  const {user, pb } = locals;
+  const {user, pb, clientIp } = locals;
   
     let output = 'failure';
       // Check if user has enough credits
+
+    const ipAddress =
+      typeof clientIp === "string" && clientIp?.trim()?.length > 0
+        ? clientIp?.trim()
+        : undefined;
+    
+
   if (user?.downloadCredits > 500) {
     return new Response(JSON.stringify({ error: "Abusive usage detected. Please read our Terms of Service to understand more." }), { status: 400 });
   }
 
 try {
 
+  
   await pb.collection('users').update(user?.id, {
-    'downloadCredits': user?.downloadCredits +1
+    'downloadCredits': user?.downloadCredits +1,
+    'ipAddress': ipAddress
   });
 
   output = 'success';
