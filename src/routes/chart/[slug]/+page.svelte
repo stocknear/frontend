59bb64eb0bebce90fb0595f3782e160f9d635f9a
MuslemRahimi <mrahimi@stocknear.com>
2026-01-11 14:35:24 +0100

    update toolbar

--- src/routes/chart/[slug]/+page.svelte
+++ src/routes/chart/[slug]/+page.svelte
@@ -4702,13 +4702,13 @@
                 </svg>
                 <!-- Dropdown Arrow -->
                 <div
-                  class="absolute -right-0.5 top-1 w-[6px] h-[8px] flex items-center justify-center opacity-0 group-hover/item:opacity-100 transition-opacity duration-150 cursor-pointer"
+                  class="absolute -right-0.5 top-1/2 -translate-y-1/2 w-[8px] h-[12px] flex items-center justify-center opacity-0 group-hover/item:opacity-100 transition-opacity duration-150 cursor-pointer"
                   on:click|stopPropagation={() => openDropdownId = openDropdownId === group.id ? null : group.id}
                   role="button"
                   tabindex="0"
                   on:keydown={(e) => e.key === 'Enter' && (openDropdownId = openDropdownId === group.id ? null : group.id)}
                 >
-                  <svg viewBox="0 0 4 6" class={`w-[4px] h-[6px] fill-current transition-transform duration-200 ${openDropdownId === group.id ? 'rotate-90' : ''}`}>
+                  <svg viewBox="0 0 4 6" class={`w-[5px] h-[7px] fill-black dark:fill-white transition-transform duration-200 ${openDropdownId === group.id ? 'rotate-90' : ''}`}>
                     <path d="M1.07298,0.159458C0.827521,-0.0531526,0.429553,-0.0531526,0.184094,0.159458C-0.0613648,0.372068,-0.0613648,0.716778,0.184094,0.929388L2.61275,3.03303L0.260362,5.07061C0.0149035,5.28322,0.0149035,5.62793,0.260362,5.84054C0.505822,6.05315,0.903789,6.05315,1.14925,5.84054L3.81591,3.53075C4.01812,3.3556,4.05374,3.0908,3.92279,2.88406C3.93219,2.73496,3.87113,2.58315,3.73964,2.46925L1.07298,0.159458Z"/>
                   </svg>
                 </div>
@@ -4766,13 +4766,13 @@
               </svg>
               <!-- Dropdown Arrow -->
               <div
-                class="absolute -right-0.5 top-1 w-[6px] h-[8px] flex items-center justify-center opacity-0 group-hover/magnet:opacity-100 transition-opacity duration-150 cursor-pointer"
+                class="absolute -right-0.5 top-1/2 -translate-y-1/2 w-[8px] h-[12px] flex items-center justify-center opacity-0 group-hover/magnet:opacity-100 transition-opacity duration-150 cursor-pointer"
                 on:click|stopPropagation={() => openDropdownId = openDropdownId === "magnet" ? null : "magnet"}
                 role="button"
                 tabindex="0"
                 on:keydown={(e) => e.key === 'Enter' && (openDropdownId = openDropdownId === "magnet" ? null : "magnet")}
               >
-                <svg viewBox="0 0 4 6" class={`w-[4px] h-[6px] fill-current transition-transform duration-200 ${openDropdownId === "magnet" ? 'rotate-90' : ''}`}>
+                <svg viewBox="0 0 4 6" class={`w-[5px] h-[7px] fill-black dark:fill-white transition-transform duration-200 ${openDropdownId === "magnet" ? 'rotate-90' : ''}`}>
                   <path d="M1.07298,0.159458C0.827521,-0.0531526,0.429553,-0.0531526,0.184094,0.159458C-0.0613648,0.372068,-0.0613648,0.716778,0.184094,0.929388L2.61275,3.03303L0.260362,5.07061C0.0149035,5.28322,0.0149035,5.62793,0.260362,5.84054C0.505822,6.05315,0.903789,6.05315,1.14925,5.84054L3.81591,3.53075C4.01812,3.3556,4.05374,3.0908,3.92279,2.88406C3.93219,2.73496,3.87113,2.58315,3.73964,2.46925L1.07298,0.159458Z"/>
                 </svg>
               </div>
