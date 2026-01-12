abe1d208b0c0f1cf8a93afcc9a2c96101be2ffc5
MuslemRahimi <mrahimi@stocknear.com>
2026-01-12 10:56:16 +0100

    ui fix

--- src/routes/hedge-funds/[slug]/+page.svelte
+++ src/routes/hedge-funds/[slug]/+page.svelte
@@ -308,7 +308,7 @@
                       Main Sectors:
                     </div>
                     <div
-                      class="flex flex-wrap items-center gap-x-2 gap-y-3 justify-center sm:justify-start"
+                      class="flex flex-wrap items-center gap-x-2 gap-y-3 justify-start sm:justify-center"
                     >
                       {#each data?.getHedgeFundsData?.mainSectors as item}
                         <a
