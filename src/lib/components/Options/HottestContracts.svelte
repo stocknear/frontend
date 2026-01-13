db6343a47e2e8ef84d77bddc9f6214f83f9f4b3b
MuslemRahimi <mrahimi@stocknear.com>
2026-01-13 18:18:00 +0100

    ui fix

--- src/lib/components/Options/HottestContracts.svelte
+++ src/lib/components/Options/HottestContracts.svelte
@@ -554,7 +554,7 @@
                     <td class="text-sm text-end whitespace-nowrap">
                       <a
                         href={`/${["stocks", "stock"]?.includes(assetType) ? "stocks" : assetType === "etf" ? "etf" : "index"}/${ticker}/options/contract-lookup?contract=${item?.option_symbol}`}
-                        class="cursor-pointer text-gray-700 dark:text-zinc-200 hover:text-violet-600 dark:hover:text-violet-400 transition"
+                        class="sm:hover:text-muted dark:sm:hover:text-white text-violet-800 dark:text-violet-400 transition"
                       >
                         {item?.option_symbol}
                       </a>
