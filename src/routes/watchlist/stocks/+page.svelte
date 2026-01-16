053e811812c712375937997401f2f636cd91084c
MuslemRahimi <mrahimi@stocknear.com>
2026-01-16 13:59:53 +0100

    ui fix

--- src/routes/watchlist/stocks/+page.svelte
+++ src/routes/watchlist/stocks/+page.svelte
@@ -841,7 +841,7 @@
                 </span>
                 {#if !data?.user}
                   <a
-                    class="w-64 flex mt-3 rounded-full justify-center items-center m-auto border border-gray-900/90 dark:border-white/80 bg-gray-900 text-white dark:bg-white dark:text-gray-900 hover:bg-gray-800 dark:hover:bg-zinc-200 transition duration-150 ease-in-out group"
+                    class="w-64 flex mt-3 py-2 rounded-full justify-center items-center m-auto border border-gray-900/90 dark:border-white/80 bg-gray-900 text-white dark:bg-white dark:text-gray-900 hover:bg-gray-800 dark:hover:bg-zinc-200 transition duration-150 ease-in-out group"
                     href="/register"
                   >
                     Get Started
@@ -849,7 +849,7 @@
                       class="tracking-normal group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out"
                     >
                       <svg
-                        class="w-4 h-4"
+                        class="ml-1 size-5 text-white dark:text-gray-900"
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
                         ><g transform="rotate(90 12 12)"
@@ -857,7 +857,7 @@
                             ><path
                               d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"
                             /><path
-                              fill="black"
+                              fill="currentColor"
                               d="M13.06 3.283a1.5 1.5 0 0 0-2.12 0L5.281 8.939a1.5 1.5 0 0 0 2.122 2.122L10.5 7.965V19.5a1.5 1.5 0 0 0 3 0V7.965l3.096 3.096a1.5 1.5 0 1 0 2.122-2.122L13.06 3.283Z"
                             /></g
                           ></g
