52da0a1b153ee318cf4014cde534f96cb32dea5e
MuslemRahimi <mrahimi@stocknear.com>
2026-01-17 01:12:45 +0100

    update pro chart

--- src/routes/chart/[slug]/+page.svelte
+++ src/routes/chart/[slug]/+page.svelte
@@ -6530,6 +6530,31 @@
               <span>Save as New</span>
             </label>
           {/if}
+
+          {#if ruleOfList?.length > 0}
+            <button
+              on:click={clearIndicators}
+              class="flex items-center gap-1 px-2 py-1 text-sm font-medium text-neutral-400 hover:text-neutral-200 hover:bg-neutral-800 rounded cursor-pointer transition whitespace-nowrap"
+            >
+              <svg
+                class="h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 21 21"
+              >
+                <g
+                  fill="none"
+                  fill-rule="evenodd"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M3.578 6.487A8 8 0 1 1 2.5 10.5" />
+                  <path d="M7.5 6.5h-4v-4" />
+                </g>
+              </svg>
+              <span>Reset All</span>
+            </button>
+          {/if}
         {/if}
 
         {#if showDetailedAnalysis}
