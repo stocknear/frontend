83e7e0166ec17870af1e7f015a1c169f97bf956b
MuslemRahimi <mrahimi@stocknear.com>
2026-02-03 10:04:48 +0100

    update chart

--- src/routes/chart/[slug]/+page.svelte
+++ src/routes/chart/[slug]/+page.svelte
@@ -8728,31 +8728,27 @@
                         toolIcons.horizontalStraightLine}
                     />
                   </svg>
-                  <!-- Dropdown Arrow - hidden when drawings are locked -->
-                  {#if !drawingsLocked}
-                    <DropdownMenu.Trigger asChild let:builder>
-                      <div
-                        use:builder.action
-                        {...builder}
-                        class="absolute -right-1 top-1/2 -translate-y-1/2 w-[16px] h-[24px] flex items-center justify-center opacity-40 group-hover/item:opacity-100 hover:opacity-100 hover:bg-gray-200/50 dark:hover:bg-zinc-700/50 rounded transition-all duration-200 cursor-pointer"
-                        on:click|stopPropagation
-                      >
-                        <svg
-                          viewBox="0 0 4 6"
-                          class="w-[6px] h-[9px] fill-black dark:fill-white transition-transform duration-300 ease-out {dropdownStates[
-                            group.id
-                          ]
-                            ? 'rotate-90'
-                            : ''}"
-                        >
-                          <path
-                            d="M1.07298,0.159458C0.827521,-0.0531526,0.429553,-0.0531526,0.184094,0.159458C-0.0613648,0.372068,-0.0613648,0.716778,0.184094,0.929388L2.61275,3.03303L0.260362,5.07061C0.0149035,5.28322,0.0149035,5.62793,0.260362,5.84054C0.505822,6.05315,0.903789,6.05315,1.14925,5.84054L3.81591,3.53075C4.01812,3.3556,4.05374,3.0908,3.92279,2.88406C3.93219,2.73496,3.87113,2.58315,3.73964,2.46925L1.07298,0.159458Z"
-                          />
-                        </svg>
-                      </div>
-                    </DropdownMenu.Trigger>
-                  {/if}
                 </button>
+                <!-- Dropdown Arrow - hidden when drawings are locked -->
+                {#if !drawingsLocked}
+                  <DropdownMenu.Trigger asChild let:builder>
+                    <button
+                      use:builder.action
+                      {...builder}
+                      class="absolute -right-1 top-1/2 -translate-y-1/2 w-[14px] h-[24px] flex items-center justify-center opacity-0 group-hover/item:opacity-100 hover:bg-gray-200/70 dark:hover:bg-zinc-700/70 rounded transition-all duration-200 cursor-pointer"
+                      on:click|stopPropagation
+                    >
+                      <svg
+                        viewBox="0 0 4 6"
+                        class="w-[6px] h-[9px] fill-gray-600 dark:fill-zinc-300 transition-transform duration-300 ease-out {dropdownStates[group.id] ? 'rotate-90' : ''}"
+                      >
+                        <path
+                          d="M1.07298,0.159458C0.827521,-0.0531526,0.429553,-0.0531526,0.184094,0.159458C-0.0613648,0.372068,-0.0613648,0.716778,0.184094,0.929388L2.61275,3.03303L0.260362,5.07061C0.0149035,5.28322,0.0149035,5.62793,0.260362,5.84054C0.505822,6.05315,0.903789,6.05315,1.14925,5.84054L3.81591,3.53075C4.01812,3.3556,4.05374,3.0908,3.92279,2.88406C3.93219,2.73496,3.87113,2.58315,3.73964,2.46925L1.07298,0.159458Z"
+                        />
+                      </svg>
+                    </button>
+                  </DropdownMenu.Trigger>
+                {/if}
 
                 <!-- Dropdown Menu -->
                 <DropdownMenu.Content
@@ -8822,27 +8818,25 @@
                 <svg viewBox="0 0 28 28" class="h-6 w-6 fill-current">
                   <path d={toolIcons.magnet} />
                 </svg>
-                <!-- Dropdown Arrow - always visible for better discoverability -->
-                <DropdownMenu.Trigger asChild let:builder>
-                  <div
-                    use:builder.action
-                    {...builder}
-                    class="absolute -right-1 top-1/2 -translate-y-1/2 w-[16px] h-[24px] flex items-center justify-center opacity-40 group-hover/magnet:opacity-100 hover:opacity-100 hover:bg-gray-200/50 dark:hover:bg-zinc-700/50 rounded transition-all duration-200 cursor-pointer"
-                    on:click|stopPropagation
-                  >
-                    <svg
-                      viewBox="0 0 4 6"
-                      class="w-[6px] h-[9px] fill-black dark:fill-white transition-transform duration-300 ease-out {dropdownStates.magnet
-                        ? 'rotate-90'
-                        : ''}"
-                    >
-                      <path
-                        d="M1.07298,0.159458C0.827521,-0.0531526,0.429553,-0.0531526,0.184094,0.159458C-0.0613648,0.372068,-0.0613648,0.716778,0.184094,0.929388L2.61275,3.03303L0.260362,5.07061C0.0149035,5.28322,0.0149035,5.62793,0.260362,5.84054C0.505822,6.05315,0.903789,6.05315,1.14925,5.84054L3.81591,3.53075C4.01812,3.3556,4.05374,3.0908,3.92279,2.88406C3.93219,2.73496,3.87113,2.58315,3.73964,2.46925L1.07298,0.159458Z"
-                      />
-                    </svg>
-                  </div>
-                </DropdownMenu.Trigger>
               </button>
+              <!-- Dropdown Arrow - appears on hover -->
+              <DropdownMenu.Trigger asChild let:builder>
+                <button
+                  use:builder.action
+                  {...builder}
+                  class="absolute -right-1 top-1/2 -translate-y-1/2 w-[14px] h-[24px] flex items-center justify-center opacity-0 group-hover/magnet:opacity-100 hover:bg-gray-200/70 dark:hover:bg-zinc-700/70 rounded transition-all duration-200 cursor-pointer"
+                  on:click|stopPropagation
+                >
+                  <svg
+                    viewBox="0 0 4 6"
+                    class="w-[6px] h-[9px] fill-gray-600 dark:fill-zinc-300 transition-transform duration-300 ease-out {dropdownStates.magnet ? 'rotate-90' : ''}"
+                  >
+                    <path
+                      d="M1.07298,0.159458C0.827521,-0.0531526,0.429553,-0.0531526,0.184094,0.159458C-0.0613648,0.372068,-0.0613648,0.716778,0.184094,0.929388L2.61275,3.03303L0.260362,5.07061C0.0149035,5.28322,0.0149035,5.62793,0.260362,5.84054C0.505822,6.05315,0.903789,6.05315,1.14925,5.84054L3.81591,3.53075C4.01812,3.3556,4.05374,3.0908,3.92279,2.88406C3.93219,2.73496,3.87113,2.58315,3.73964,2.46925L1.07298,0.159458Z"
+                    />
+                  </svg>
+                </button>
+              </DropdownMenu.Trigger>
 
               <!-- Magnet Mode Dropdown -->
               <DropdownMenu.Content
