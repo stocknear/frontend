c1b8d83d6ce82d009a45cd092faef00dbd4387c2
MuslemRahimi <mrahimi@stocknear.com>
2026-02-15 12:34:04 +0100

    ui fix

--- src/routes/options-flow/+page.svelte
+++ src/routes/options-flow/+page.svelte
@@ -1999,10 +1999,31 @@
                     <DropdownMenu.Group>
                       {#each popularStrategyList as item}
                         <DropdownMenu.Item
-                          on:click={() => applyPopularStrategy(item?.key)}
+                          on:click={() => {
+                            if (data?.user?.tier !== "Pro") {
+                              goto("/pricing");
+                            } else {
+                              applyPopularStrategy(item?.key);
+                            }
+                          }}
                           class="cursor-pointer sm:hover:text-violet-800 dark:sm:hover:text-violet-400"
                         >
                           {item?.label}
+                          {#if data?.user?.tier !== "Pro"}
+                            <svg
+                              class="ml-1 size-4"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              style="max-width: 40px;"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                clip-rule="evenodd"
+                              >
+                              </path>
+                            </svg>
+                          {/if}
                         </DropdownMenu.Item>
                       {/each}
                     </DropdownMenu.Group>
