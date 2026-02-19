f2ea5ba912570d5f025618f72878c82bd284d943
MuslemRahimi <mrahimi@stocknear.com>
2026-02-19 09:55:15 +0100

    bugfixing

--- src/routes/chat/+layout.server.ts
+++ src/routes/chat/+layout.server.ts
@@ -7,18 +7,14 @@ export const load = async ({ locals }) => {
     const output = await pb.collection("chat").getList(1, 50, {
       sort: "-updated",
       filter: `user="${user.id}"`,
+      fields: "id,updated,messages",
     });
 
-    return output.items.map((chat) => {
-      const firstUserMessage =
-        chat.messages?.find((m) => m.role === "user")?.content || null;
-
-      return {
-        id: chat?.id,
-        updated: chat?.updated,
-        message: firstUserMessage,
-      };
-    });
+    return output.items.map((chat) => ({
+      id: chat?.id,
+      updated: chat?.updated,
+      message: chat?.messages?.at(0)?.content?.slice(0, 100) || null,
+    }));
   };
 
   return {
